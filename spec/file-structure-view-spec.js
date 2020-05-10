7cc280a5f1978342d34274db1b469fc5c93efa91
Shukant Pal <shukantpal@outlook.com>
2020-05-09 23:54:58 -0400

    Initial work on BabelBuilder + CSymbolTree + docs

---
+++ spec/file-structure-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FileStructureView from '../lib/file-structure-view';
+
+describe('FileStructureView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
