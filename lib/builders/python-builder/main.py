212016fb8a5a945c4ee6526318235052d7c99df4
iamPres <prestowillis@gmail.com>
2020-05-25 14:11:49 -0500

    added dependencies and basic class hierarchy

--- lib/builders/python-builder/main.py
+++ lib/builders/python-builder/main.py
@@ -6,3 +6,18 @@ def func1():
     def func2():
         var2 = 6
     cde = "hii"
+
+class Cls:
+    property = "value"
+    bool = False
+    num = 5
+    def __init__(self):
+        self.declProperty = "VAL"
+
+    def method1(self):
+        vari = 8
+        vars = "string"
+    def method2(self):
+        print("hi again")
+
+Cls.x = "vla"
